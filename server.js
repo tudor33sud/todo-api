1c06e878b52ad76ba483231e7e9a224331866384
unknown <z003jn9v@MD1HCHSC.ww300.siemens.net>
2016-03-28 17:43:05 +0300

    Add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-},{
-	id: 2,
-	description: 'Go to market',
-	completed: false
-},{
-	id: 3,
-	description: 'Check your emails',
-	completed:true
-}];
+var todos = [];
+var todoNextId = 1 ;
+
+app.use(bodyParser.json());
 
 app.get('/',function(req, res){
 	res.send('Todo API Root');
@@ -25,7 +17,7 @@ app.get('/todos',function(req, res){
 });
 
 // GET /todos/:id
-app.get('/todos/:id',function(req,res){
+app.get('/todos/:id', function(req,res){
 	var todoId = parseInt(req.params.id,10);
 	var matchingTodo;
 	todos.forEach(function(todo){
@@ -40,6 +32,17 @@ app.get('/todos/:id',function(req,res){
 	}	
 
 });
+
+// POST /todos 
+app.post('/todos', function(req,res) {
+	var body = req.body;
+	//add id field
+	body.id = todoNextId++;
+	//push body into array
+	todos.push(body);
+	
+	res.json(body);
+})
 
 
 app.listen(PORT, function(){
