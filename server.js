8c88d1de7f000021bb0a5927c9d68403aca545b9
unknown <z003jn9v@MD1HCHSC.ww300.siemens.net>
2016-03-28 17:01:11 +0300

    changed port variable

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Meet mom for lunch',
+	completed: false
+},{
+	id: 2,
+	description: 'Go to market',
+	completed: false
+}];
 
 app.get('/',function(req, res){
 	res.send('Todo API Root');
