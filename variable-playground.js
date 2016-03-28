1227f2690b7c7f08aac18a216dbe717babfde6ae
unknown <z003jn9v@MD1HCHSC.ww300.siemens.net>
2016-03-28 18:56:31 +0300

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,24 @@
+// var person = {
+// 	name:'Andrew',
+// 	age:21
+// };
+
+// function updatePerson (obj){
+// 	// obj = {
+// 	// 	name: 'Andrew',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//Array example 
+var grades = [15,37];
+function addGrades (gradesArr) {
+	gradesArr.push(55);
+	debugger;
+}
+addGrades(grades);
+console.log(grades);
