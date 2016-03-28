af89f49779319f63d589e679c4b0940bd94a3122
unknown <z003jn9v@MD1HCHSC.ww300.siemens.net>
2016-03-28 18:02:08 +0300

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -19,14 +21,10 @@ app.get('/todos',function(req, res){
 // GET /todos/:id
 app.get('/todos/:id', function(req,res){
 	var todoId = parseInt(req.params.id,10);
-	var matchingTodo;
-	todos.forEach(function(todo){
-		if(todo.id === todoId){
-			matchingTodo = todo;
-		}
-	});
-	if(matchingTodo){
-		res.json(matchingTodo);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	
+	if(matchedTodo){
+		res.json(matchedTodo);
 	} else{
 		res.status(404).send();	
 	}	
@@ -35,7 +33,14 @@ app.get('/todos/:id', function(req,res){
 
 // POST /todos 
 app.post('/todos', function(req,res) {
-	var body = req.body;
+	var body = _.pick(req.body,'description','completed');
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+
 	//add id field
 	body.id = todoNextId++;
 	//push body into array
