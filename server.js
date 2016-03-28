39c5811549f7b198e925c3dbfd1740abed425f93
unknown <z003jn9v@MD1HCHSC.ww300.siemens.net>
2016-03-28 18:56:09 +0300

    Add ?completed filter

--- server.js
+++ server.js
@@ -15,7 +15,21 @@ app.get('/',function(req, res){
 
 // GET /todos
 app.get('/todos',function(req, res){
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos ;
+
+	if(queryParams.hasOwnProperty('completed')){
+		
+		if(queryParams.completed === 'true') {
+			filteredTodos = _.where(filteredTodos, {completed:true});
+		} else if (queryParams.completed === 'false') {
+			filteredTodos = _.where(filteredTodos, {completed:false});
+		} else{
+			res.status(400).send();
+			return;
+		}
+	}
+	res.json(filteredTodos);
 });
 
 // GET /todos/:id
