03e87c063f3444833c07e77946fc8094fc971d9f
unknown <z003jn9v@MD1HCHSC.ww300.siemens.net>
2016-03-28 17:17:50 +0300

    Added todos and /todos/:id

--- server.js
+++ server.js
@@ -9,11 +9,38 @@ var todos = [{
 	id: 2,
 	description: 'Go to market',
 	completed: false
+},{
+	id: 3,
+	description: 'Check your emails',
+	completed:true
 }];
 
 app.get('/',function(req, res){
 	res.send('Todo API Root');
 });
+
+// GET /todos
+app.get('/todos',function(req, res){
+	res.json(todos);
+});
+
+// GET /todos/:id
+app.get('/todos/:id',function(req,res){
+	var todoId = parseInt(req.params.id,10);
+	var matchingTodo;
+	todos.forEach(function(todo){
+		if(todo.id === todoId){
+			matchingTodo = todo;
+		}
+	});
+	if(matchingTodo){
+		res.json(matchingTodo);
+	} else{
+		res.status(404).send();	
+	}	
+
+});
+
 
 app.listen(PORT, function(){
 	console.log('Express listening on port: '+ PORT + '!');
