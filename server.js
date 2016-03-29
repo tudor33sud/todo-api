b0ddc502da18a7d5ccf752728447a8299455f07b
unknown <z003jn9v@MD1HCHSC.ww300.siemens.net>
2016-03-29 16:48:29 +0300

    add user model and post /users request

--- server.js
+++ server.js
@@ -134,6 +134,16 @@ app.put('/todos/:id', function(req, res) {
 	});
 });
 
+app.post('/users', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.create(body).then(function(user) {
+		res.json(user);
+	}, function(error) {
+		res.status(400).json(error);
+	});
+});
+
 db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port: ' + PORT + '!');
